a277f96d7af9caae4a7c1550b94db8909b4a82da
Xophaie <grogew@mail.ru>
2024-05-24 06:20:13 +0300

    3rd night

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
-	$(".navigation__open-button").on("click", function () {
-		$(".navigation__menu").toggleClass("d-none");
-		$(".menu-toggle").toggleClass("open");
+	$(".input-phone")
+		.toArray()
+		.forEach(element => {
+			const cleave = new Cleave(element, {
+				phone: true,
+				phoneRegionCode: "ru",
+			});
+		});
+
+	$("div.documents__item").on("click", function (e) {
+		e.stopPropagation();
+		// const img = $(this)[0];
+
+		if (e.target.classList.contains("zoom")) {
+			$(this).removeClass("zoom");
+			$(this).children(".documents__img").removeClass("scale");
+			$("body").removeClass("modal-open");
+		} else {
+			$(this).addClass("zoom");
+			$("body").addClass("modal-open");
+		}
+	});
+
+	$(".documents__img").on("click", function (e) {
+		console.log(e.target);
+		if (e.target.parentElement.classList.contains("zoom")) {
+			e.target.classList.toggle("scale");
+		}
 	});
 });
